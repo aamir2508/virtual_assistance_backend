313479f51ab93a37c6f5f907d100a22f9aea60ba
aamir2508 <aamir2508@yahoo.co.in>
2022-01-10 15:09:51 +0530

    Added cors for aws saving video

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 5000 ;
 const app = express();
 const cors = require("cors");
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  next();
+});
 const mysql = require("mysql")
 
 // const DB_HOST = process.env.DB_HOST
